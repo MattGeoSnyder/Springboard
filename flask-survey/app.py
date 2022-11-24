4ef88b85ad723bfccdfde259de4bcde8374a2f42
mattgeosnyder <mattgeosnyder@gmail.com>
2022-11-23 20:29:20 -0500

    Commiting flask-survey for submission

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, flash, request
+from flask import session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,16 +8,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "$Boy07032018"
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 
 @app.route('/')
 def home():
     return render_template('survey.html', survey=satisfaction_survey)
 
 
+@app.route('/start-session', methods=['POST'])
+def initiate_sesion():
+    session['responses'] = []
+    return redirect('/questions/0')
+
 @app.route('/questions/<int:q_num>')
 def go_to_question(q_num):
+    responses = session['responses']
     if q_num > len(responses):
         flash("You must answer the questions in order")
         return redirect(f'/questions/{len(responses)}')
@@ -29,5 +34,8 @@ def go_to_question(q_num):
 
 @ app.route('/answer', methods=['POST'])
 def add_answer():
+    responses = session['responses']
+    print(responses)
     responses.append(request.form[f'{len(responses)}_ans'])
+    session['responses'] = responses
     return redirect(f'/questions/{len(responses)}')
