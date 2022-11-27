955f1e272a39aba33e75d8042c6bbb18e80dd60e
MattGeoSnyder <mattgeosnyder@gmail.com>
2022-11-27 01:46:27 -0500

    Finished main features. Time to reafactor and style

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -1,9 +1,7 @@
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, session
-from flask import flash, jsonify
+from flask import Flask, request, render_template, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 import pdb
-
 
 
 boggle_game = Boggle()
@@ -14,16 +12,31 @@ app.config['SECRET_KEY'] = "$Boy07032018"
 toolbar = DebugToolbarExtension()
 toolbar.__init__(app)
 
+
 @app.route('/')
 def display_board():
     board = boggle_game.make_board()
     session['board'] = board
     return render_template('root.html', board=board)
 
+
 @app.route("/answer", methods=["POST"])
 def check_answer():
     guess = request.json['answer']
     board = session['board']
     result = boggle_game.check_valid_word(board, guess)
-    flash(result)
     return jsonify({"result": result})
+
+
+@app.route('/stats', methods=['POST'])
+def update_stats():
+    games_played = session.get('games_played', 0)
+    games_played += 1
+    session['games_played'] = games_played
+    high_score = session.get('high_score', 0)
+    score = request.json['score']
+    if score > high_score:
+        high_score = score
+        session['high_score'] = high_score
+    stats = {'games_played': games_played, 'high_score': high_score}
+    return jsonify(stats)
--- flask-boggle/static/answer.js
+++ flask-boggle/static/answer.js
@@ -1,29 +1,43 @@
-$form = $("form");
-scoreSection = document.querySelector('#score-section')
-timerSection = document.querySelector('#timer')
+let $form = $("form");
+let scoreSection = document.querySelector('#score-section');
+let timerSection = document.querySelector('#timer');
+let highScoreSection = document.querySelector('#high-score');
+let gamesPlayedSection = document.querySelector('#games-played');
 
 let score = 0;
 let time = 60;
-scoreSection.innerText = `${score}`
 
-timerSection.addEventListener('DOMContentLoaded',function(e){
-    setInterval(function() {
-        time -= 1;
-        timerSection.innerText = `${time}`;
-    }, 1000);
-})
+scoreSection.innerText = `${score}`
+timerSection.innerText = `${time}`
+
+document.addEventListener('DOMContentLoaded',function(e) {
+    let int_id = setInterval(async function() {
+        if(time > 0){
+            time -= 1;
+            timerSection.innerText = `${time}`;    
+        } else {
+            $form.find('input').prop('disabled', true);
+            $form.find('button').prop('disabled', true);    
+            let req = await updateStats();
+            console.log('hello');
+            updateStatsBoard(req);
+            clearInterval(int_id);
+        }
+    }, 1000);               
+});
+
 
 $form.on('submit', async function(e){
     e.preventDefault()
     let guess = $form.find('input').val();
-    let req = await sendAxiosReq(guess);
+    let req = await sendAnswer(guess);
     updateResultSection(req.data['result']);
     updateScoreSection(req.data['result'], guess);
     $form.find('input').val("");
 });
 
-async function sendAxiosReq(answer){
-    req = await axios.post('/answer', {answer});
+async function sendAnswer(answer){
+    let req = await axios.post('/answer', {answer});
     return req;
 }
 
@@ -41,6 +55,19 @@ function updateScore(result, guess){
     if(result === 'ok'){
         score += guess.length;
     }
+}
+
+async function updateStats() {
+    let req = await axios.post('/stats', {"score": score});
+    return req;
+}
+
+function updateStatsBoard(req) {
+    let stats = req.data;
+    let highScore = stats['high_score'];
+    let gamesPlayed = stats['games_played'];
+    highScoreSection.innerText = `${highScore}`;
+    gamesPlayedSection.innerText = `${gamesPlayed}`;
 }
 
 
