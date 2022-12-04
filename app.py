1469fc427803f223ea7061beacbaadc5dcd0152c
MattGeoSnyder <mattgeosnyder@gmail.com>
2022-12-04 10:42:10 -0500

    Added comments to Python files.

--- app.py
+++ app.py
@@ -12,13 +12,18 @@ def get_root():
 
 @app.route('/convert')
 def convert_currency():
+    # Get inputs from query string
     from_curr = request.args['from-currency']
     to_curr = request.args['to-currency']
     amount = request.args['amount']
+
+    # Calls conversion from forex.py
     conversion = convert(from_curr, to_curr, amount)
 
+    # Redirects to root in case of error. Throws flash messages on root.
     if conversion['status'] != 'okay':
         flash(conversion['status'], 'Error')
         return redirect('/')
     
+    # If conversion status is okay, returns convert page displaying conversion amount
     return render_template('convert.html', conversion=conversion)
--- forex.py
+++ forex.py
@@ -2,25 +2,51 @@ from forex_python.converter import CurrencyRates, CurrencyCodes
 from forex_python.converter import RatesNotAvailableError, DecimalFloatMismatchError
 from datetime import datetime
 
+# Initialize objects from Forex module
 c = CurrencyRates()
 codes = CurrencyCodes()
 
 def convert(curr1, curr2, amount):
+    """
+        Args: str:curr1, str:curr2, num:amount
+
+        Takes two currencies and converts the amount of curr1 into curr2.
+
+        Return: 
+            Dict: {
+                'status': str: description of error type if any, else 'okay',
+                'conversion': num: amount of curr2 given amount of curr1,
+                'symbol': str: currency symbol of curr2
+            }
+
+
+    """
+
+    # Input handling. Inputs must be in all upper case.
     curr1 = curr1.upper()
     curr2 = curr2.upper()
 
+    # Initialize variables for return dictionary, since they are set in try
+    # block
     conversion = None
     status = None
     symbol = None
+
+    # Try block handling various errors from module.
     try:
+        # gets conversion amount, currency symbol and sets status to okay
         conversion = c.convert(curr1, curr2, amount)
         symbol = codes.get_symbol(curr2)
         status = 'okay'
     except RatesNotAvailableError:
+        # Forex error. Comes from unrecognized input
         status = "Invalid currency"
     except DecimalFloatMismatchError:
+        # Forex error. Conversion error between two currencies. Developer 
+        # of module is working to fix.
         status = "Try a different conversion"
     except ValueError:
+        # Amount input is not a number. Occurs if amount field is left blank.
         status = 'Invalid amount'
 
     return {"status": status, "conversion": conversion, "symbol": symbol}
