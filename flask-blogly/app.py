ac1e97e28c1c0012a1e5f590e06fd00d187eb6be
MattGeoSnyder <mattgeosnyder@gmail.com>
2022-12-18 23:28:56 -0500

    Started flask-blogly exercise. Cannot get app to start.

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,49 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash
 from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+from models import Users
+
+default_image_url = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
 
 app = Flask(__name__)
+app.debug = True
+
+app.config['SECRET_KEY'] = '$Boy07032018'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+toolbar = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def get_root():
+    users = Users.query.all()
+    render_template('root.html', users)
+
+@app.route('/new-user')
+def get_add_user_page():
+    render_template('new-user.html')
+
+@app.route('new-user', methods=['POST'])
+def add_new_user():
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+    image_url = image_url if image_url else default_image_url
+
+    new_user = Users(first_name=first_name, 
+                    last_name=last_name, 
+                    image_url=image_url)
+
+    flash("User added!")
+
+    return redirect('new-user')
+
+
+
+
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -5,3 +5,9 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
