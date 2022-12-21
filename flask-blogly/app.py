65e577ac1c63e4fa655f67121d1850251e4734c2
MattGeoSnyder <mattgeosnyder@gmail.com>
2022-12-20 23:10:19 -0500

    Finished all routes. Need to add testing.

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, render_template, redirect, flash
-from models import db, connect_db, Users
+from models import db, connect_db, Users, default_image_url
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -20,29 +20,63 @@ with app.app_context():
     db.create_all()
 
 @app.route('/')
+def go_to_users():
+    return redirect('/users')
+
+@app.route('/users')
 def get_root():
     users = Users.query.all()
     return render_template('root.html', users=users)
 
-@app.route('/new-user')
+@app.route('/users/new')
 def get_add_user_page():
     return render_template('new-user.html')
 
-@app.route('/new-user', methods=['POST'])
+@app.route('/users/new', methods=['POST'])
 def add_new_user():
     first_name = request.form['first-name']
     last_name = request.form['last-name']
     image_url = request.form['image-url']
     image_url = image_url if image_url else default_image_url
 
-    new_user = Users(first_name=first_name, 
-                    last_name=last_name, 
-                    image_url=image_url)
+    new_user = Users(first_name=first_name, last_name = last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
 
     flash("User added!")
 
-    return redirect('new-user')
+    return redirect('/users')
 
+@app.route('/users/<int:user_id>')
+def display_user(user_id):
+    user = Users.query.get_or_404(user_id)
+    return render_template('user.html', user=user)
 
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    user = Users.query.get_or_404(user_id)
+    return render_template('edit.html',user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def post_edit(user_id):
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+    image_url = image_url if image_url else default_image_url
+
+    user = Users.query.get_or_404(user_id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    user = Users.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
 
 
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -11,10 +11,7 @@ def connect_db(app):
 class Users(db.Model):
     __tablename__ = 'users'
 
-    def __init__(self):
-        self.default_image_url = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
-    image_url = db.Column(db.String, nullable=False, default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')
+    image_url = db.Column(db.String, nullable=False, default=default_image_url)
