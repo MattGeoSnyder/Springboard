1594f2de039b02045c76d386f62406da1fdd794c
MattGeoSnyder <mattgeosnyder@gmail.com>
2023-04-22 16:28:53 -0400

    Finished module exercise.

--- module_exercise/src/index.js
+++ module_exercise/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {choice, remove} from './helpers.js';
+import foods from './foods.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +17,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let fruit = choice(foods);
+console.log(`I'd like one ${fruit}, please`);
+console.log(`Here you go: ${fruit}`);
+console.log('Delicious! May I have another');
+remove(foods, fruit);
+console.log(`I'm sorry, we're all out. We have ${foods} left`);
