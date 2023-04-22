757e45b91fc2284e85406b5bc70680304ccd95fb
MattGeoSnyder <mattgeosnyder@gmail.com>
2023-04-22 17:41:03 -0400

    Committing Pokedex exercise for submission.

---
+++ Pokedex/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign: 'center'}}>Pokedex</h1>
+      <Pokedex></Pokedex>
+    </>
+  );
+}
+
+export default App;
