cfc2bad0514b767344704a2f4a46ba6f56a38534
mattgeosnyder <mattgeosnyder@gmail.com>
2023-04-27 16:15:19 -0400

    Finished with parts 2 & 3 of Assessment8

--- Assessment8/add-commas/addCommas.js
+++ Assessment8/add-commas/addCommas.js
@@ -1,24 +1,30 @@
 function addCommas(num) {
-    let numStr = String(num).split();
-    let startIdx = numStr.indexOf('.');
-    let end = numStr.splice(startIdx);
-    startIdx--;
-    let idx = startIdx;
+    let numStr = String(num).split("");
+    let idx = numStr.indexOf('.');
+    let end;
+
+    if (idx > 0) {
+        end = numStr.splice(idx)
+    } else {
+        idx = numStr.length;
+    }
+
+    idx--;
+    let count = 1;
     while (idx > 0) {
-        if (idx % 3 === 0 && idx < startIdx) {
-            numStr.splice(idx, 0, ','); 
+        if (numStr[idx-1] === '-'){
+            break;
+        }
+
+        if (count === 3) {
+            numStr.splice(idx, 0, ',');
+            count = 0;
         }
         idx--;
+        count++;
     }
     let ret = numStr.concat(end);
-    return ret.join()
+    return ret.join('')
 }
 
-console.log(addCommas(1234));
-console.log(addCommas(1000000));
-console.log(addCommas(9876543210));
-console.log(addCommas(6));
-console.log(addCommas(-10));
-console.log(addCommas(-5678));
-console.log(addCommas(12345.678));
-console.log(addCommas(31414592.65));
+module.exports = addCommas;
