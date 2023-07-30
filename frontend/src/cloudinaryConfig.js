76565e661c25edadd4ef0d6fd802f43aaab8f602
MattGeoSnyder <mattgeosnyder@gmail.com>
2023-07-29 21:11:49 -0400

    Changes made for deployment.

--- frontend/src/cloudinaryConfig.js
+++
@@ -1,8 +0,0 @@
-import env from 'dotenv'
-
-env.config()
-
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUD_NAME;
-const CLOUDINARY_API_KEY = process.env.API_KEY;
-
-export { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY };
